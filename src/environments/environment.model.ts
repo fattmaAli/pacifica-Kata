773ed9ff519934065d58d53b071c539fbedb4a34
fattmaAli <alifatma140194@gmail.com>
2022-12-22 13:31:09 +0100

    Updating envioronments

--- src/environments/environment.model.ts
+++ src/environments/environment.model.ts
@@ -1,10 +1,10 @@
-import { Type } from "@angular/core";
-import { ApiUrlService } from "@providers/api/api-url.service";
-import { HttpApiProvider } from "@providers/http/http-api.provider";
+import {Type} from "@angular/core";
+import {ApiUrlService} from "@providers/api/api-url.service";
+import {HttpApiProvider} from "@providers/http/http-api.provider";
 
 export interface Environment {
-    production: boolean;
-    apiUrlServiceClass: Type<ApiUrlService>;
-    httpApiProviderClass: Type<HttpApiProvider>;
+  production: boolean;
+  apiUrlServiceClass: Type<ApiUrlService>;
+  httpApiProviderClass: Type<HttpApiProvider>;
 
-  }
+}
